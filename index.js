121da70ba968d131b8bd18184469d6ad90a2b420
Vaishnavi685 <142629353+Vaishnavi685@users.noreply.github.com>
2026-01-09 22:06:02 +0530

    Fix Render port issue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 
-const port = 4000
+const PORT = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -19,7 +19,7 @@ app.get('/youtube',(req,res)=>{
     res.send('<h2>hey! you are on youtube</h2>')
 })
 
-app.listen(process.nextTick.PORT, () => {
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
